806f9df9436dcf434f88f67e95e58da11b49152a
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-26 22:26:39 -0800

    adding true to resizable activity

--- app/src/main/java/com/example/groundcontrolsystem/MainActivity.kt
+++ app/src/main/java/com/example/groundcontrolsystem/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GroundControlSystemTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
