7c68499f8c6845e9428fc584c19098045eb4da0a
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-27 00:39:51 -0800

    fixed bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    
+    // Icons
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+
     implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
