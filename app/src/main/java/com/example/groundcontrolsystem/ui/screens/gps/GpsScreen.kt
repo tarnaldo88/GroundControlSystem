ff28396380ee45874d141bfb54d1f42ed23c3cf6
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-27 16:32:32 -0800

    adding in GoogleMap

--- app/src/main/java/com/example/groundcontrolsystem/ui/screens/gps/GpsScreen.kt
+++ app/src/main/java/com/example/groundcontrolsystem/ui/screens/gps/GpsScreen.kt
@@ -1,18 +1,30 @@
 package com.example.groundcontrolsystem.ui.screens.gps
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun GpsScreen() {
-    Card(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text("GPS Page")
-        }
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
     }
 }
