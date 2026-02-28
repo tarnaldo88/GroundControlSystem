f67b2905441150c8b9adac6a04df547e1ff4d4bc
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-28 10:21:26 -0800

    starting to setup camera screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,12 @@ dependencies {
     // OpenStreetMap
     implementation(libs.osmdroid)
 
+    // CameraX
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/groundcontrolsystem/ui/screens/camera/CameraScreen.kt
+++ app/src/main/java/com/example/groundcontrolsystem/ui/screens/camera/CameraScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
